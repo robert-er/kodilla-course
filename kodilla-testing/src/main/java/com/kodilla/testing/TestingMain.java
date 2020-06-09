1fb2d86609e429ab913b4f4143c9f736a9bcc132
robert-er <robert.roza.pl@gmail.com>
2020-06-09 19:33:23 +0200

    modul6.2 testy kalkulatora

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(1,3) == 4) {
+            System.out.println("calculator, method 'add' OK");
+        } else {
+            System.out.println("calculator, method 'add' Error");
+        }
+
+        if (calculator.subtract(9, 4) == 5) {
+            System.out.println("calculator, method 'subtract' OK");
+        } else {
+            System.out.println("calculator, method 'subtract' Error");
+        }
     }
 }

3c6fdb342f39324c3149d17b31d2c83bf546acc9
robert-er <robert.roza.pl@gmail.com>
2020-06-13 19:46:42 +0200

    fibonaci test

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/fibonacci/FibonacciTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.testing.collection.fibonacci;
+
+import com.kodilla.testing.fibonacci.Fibonacci;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciTestSuite {
+
+    @Test
+    public void testZero() {
+        Fibonacci fibonacci = new Fibonacci();
+        Assert.assertEquals(0, fibonacci.f(0));
+    }
+
+    @Test
+    public void testOne() {
+        Fibonacci fibonacci = new Fibonacci();
+        Assert.assertEquals(1, fibonacci.f(1));
+    }
+
+    @Test
+    public void testTwelve() {
+        Fibonacci fibonacci = new Fibonacci();
+        Assert.assertEquals(144, fibonacci.f(12));
+    }
+
+}

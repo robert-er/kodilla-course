937d0f58acd9484db158992eafe1e53862bb05c9
robert-er <robert.roza.pl@gmail.com>
2020-06-19 22:07:54 +0200

    modul 7.3 zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,29 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+/*import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import com.kodilla.stream.person.People;*/
+
+
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        /*System.out.println("Welcome to module 7 - Stream");
 
         SaySomething saySomething = new SaySomething();
         saySomething.say();
@@ -52,6 +68,64 @@ public class StreamMain {
             }
             return result.toString();
         };
-        poemBeautifier.beautify(baseText, lambda);
+        poemBeautifier.beautify(baseText, lambda);*/
+
+        /*  modul 7.3 - kurs
+        People.getList().stream()
+
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.startsWith("M"))
+                .forEach(System.out::println);
+        //-------------------------------------------------------------
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+        //-------------------------------------------------------------
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks
+                .forEach(System.out::println);
+        //-------------------------------------------------------------
+
+        BookDirectory theBookDirectory = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+        //-------------------------------------------------------------
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<\n","\n>>"));
+
+        System.out.println(theResultStringOfBooks);
+        */
+
+        // Modul 7.3 Zadanie: funkcyjny spacer po liście użytkowników forum
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> userMap = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getBirthDate().isBefore(LocalDate.of(LocalDate.now().getYear()-20,
+                        LocalDate.now().getMonth(), LocalDate.now().getDayOfMonth())))
+                .filter(forumUser -> forumUser.getPostCounter() > 0)
+                .collect(Collectors.toMap(ForumUser::getId, forumUser -> forumUser));
+
+        userMap.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }

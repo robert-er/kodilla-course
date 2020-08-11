01852ce2f6ce9ebb25e4458a7cdba19741f18352
robert-er <robert.roza.pl@gmail.com>
2020-08-11 18:34:58 +0200

    zadanie15.1

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -0,0 +1,30 @@
+package com.kodilla.patterns.singleton;
+
+public final class Logger {
+
+    private static Logger logger = null;
+    private String lastLog = "";
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (logger == null) {
+            synchronized(SettingsFileEngine.class) {
+                if (logger == null) {
+                    logger = new Logger();
+                }
+            }
+        }
+        return logger;
+    }
+
+    public void log(String log) {
+        lastLog = log;
+        System.out.println("Log: [" + log + "]");
+    }
+
+    public String getLastLog() {
+        return lastLog;
+    }
+}

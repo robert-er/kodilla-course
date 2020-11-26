9b684c865be890da79f95cef5955c4e83ab1ceef
robert-er <robert.roza.pl@gmail.com>
2020-11-26 18:35:10 +0100

    zadanie30.3 testUpdateBestsellers

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -29,4 +29,24 @@ public class StoredProcTestSuite {
         }
         assertEquals(0, howMany);
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE books SET bestseller=false";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlProcedureCall = "CALL UpdateBestseller()";
+        statement.execute(sqlProcedureCall);
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM books WHERE bestseller=false";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if(rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(1, howMany);
+    }
 }
